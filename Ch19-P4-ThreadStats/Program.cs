0bd0c3d9a9e451b3590da4bb0714f52c2b1c66f5
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-30 12:34:42 +0500

    FINAL UPDATE

--- Ch19-P4-ThreadStats/Program.cs
+++ Ch19-P4-ThreadStats/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ch19_P4_ThreadStats
@@ -10,6 +11,19 @@ namespace Ch19_P4_ThreadStats
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Primary Thread stats *****\n");
+            // Obtain and name the current thread.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+            // Show details of hosting AppDomain/Context.
+            Console.WriteLine("Name of current AppDomain: {0}",Thread.GetDomain().FriendlyName);
+            Console.WriteLine("ID of current Context: {0}",  Thread.CurrentContext.ContextID);
+            // Print out some stats about this thread.
+            Console.WriteLine("Thread Name: {0}",  primaryThread.Name);
+            Console.WriteLine("Has thread started?: {0}",primaryThread.IsAlive);
+            Console.WriteLine("Priority Level: {0}", primaryThread.Priority);
+            Console.WriteLine("Thread State: {0}", primaryThread.ThreadState);
+            Console.ReadLine();
         }
     }
 }
